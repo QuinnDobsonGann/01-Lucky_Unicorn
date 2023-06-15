b5242c3d84b3ff05a0f8e3ed77e43cfa4d1f1dc9
Quinn Dobson_Gann <quinndobsongann@mmc.school.nz>
2023-06-16 09:40:13 +1200

    yaga

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -1,16 +1,64 @@
-
-
 # Functions go here...
-def yes_no(question):...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
 
 
-def instructions():...
+
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+
+        else:
+            print("Please answer yes / no")
+
+
+def instructions():
+    print("**** How to play ****")
+    print()
+    print("The rules of the game go here")
+    print()
+    return ""
+
+
+def num_check(question, low, high):
+    error = "Please enter an whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            response = int(input(question))
+            # if the amount is too low / too high give
+            if low < response <= high:
+                return response
+
+                # output an error
+            else:
+                print(error)
+
+
+        except ValueError:
+            print(error)
 
 
 # Main Routine goes here...
-show_instructions = yes_no("Have you played the game before")
+played_before = yes_no("have you played the"
+                       " game before? ")
 
-if show_instructions == "yes":
+if played_before == "no":
     instructions()
-else:
-    print("Program Continues")
+
+print()
+
+# Ask user how much they want to play with...
+how_much = num_check("How much would you like to play with? ", 0, 10)
+
+print("You will be spending ${}".format(how_much))
